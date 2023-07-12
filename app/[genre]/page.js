24218f2933f8a4f840da5868393b6d132956a90e
Yaslin Carmona <yaslincarmona@Yaslins-MacBook-Air.local>
2023-07-12 15:38:52 -0400

    CREATED Next.js app

--- app/[genre]/page.js
+++ app/[genre]/page.js
@@ -1,9 +1,29 @@
+import MovieScroll from "../shared/MovieScroll"
+
+export function generateStaticParams() {
+    return [
+        { genre: "comedy" },
+        { genre: "drama" },
+        { genre: "horror" },
+    ]
+}
+
+export function generateMetadata({ params: { genre } }) {
+    return {
+        title:`My ${genre} ,ovie database`,
+        description: `Here are some of my favorite ${genre} movies.`,
+
+    }
+}
+
 export default function Page ({ params: { genre } }) {
     return (
         <main className="p-8"> 
         <h1 className="capitalize text-2xl text-yellow-400 font-semibold">
             {genre} Movies
         </h1>
+        <MovieScroll genre={genre} number={12}/>
         </main>
     )
 }
+
